6266519842d833ea7948494ee96b0e08ac5c4d01
testGajjar <163375920+testGajjar@users.noreply.github.com>
2024-12-30 14:21:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def create_task(message):
     task = {
         'http_request': {
             'http_method': tasks_v2.HttpMethod.POST,
-            'url': 'https://retrieve-datetime-uuid-537715546935.us-west1.run.app',  # Replace with your actual Cloud Run service URL
+            'url': 'https://migratefromlambda-2-537715546935.us-west1.run.app',  # Replace with your actual Cloud Run service URL
             'headers': {'Content-type': 'application/json'},
             'body': json.dumps(message).encode()
         }
